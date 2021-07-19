d661fb2f930f11dec124ae3e8743d5f6d6127006
GasGas7 <79295810+GasGas7@users.noreply.github.com>
2021-07-19 16:16:58 +0200

    Initial commit

---
+++ app/Http/Controllers/TravelController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Travel;
+use Illuminate\Http\Request;
+
+class TravelController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $travels= Travel::all();
+        return view('travels.index', compact('travels'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Travel  $travel
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Travel $travel)
+    {
+        return view('travels.show', compact('travels'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Travel  $travel
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Travel $travel)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Travel  $travel
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Travel $travel)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Travel  $travel
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Travel $travel)
+    {
+        //
+    }
+}
---
+++ resources/views/home.blade.php
@@ -0,0 +1,5 @@
+@extends('layout.app')
+
+@section('content')
+    <h1>Home Page</h1>
+@endsection
